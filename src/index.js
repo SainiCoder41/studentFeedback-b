da9e44f60778cef21dc016b190c7caddd7b524d3
SainiCoder41 <sainicoder41@gmail.com>
2025-08-22 09:28:32 +0530

    First1

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/",authRouter);
+app.use("/user",authRouter);
 
 app.use("/course",Courserouter);
 app.use("/feedback",Feedback);
